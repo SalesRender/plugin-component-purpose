c222b1aa9ed92c44c6f6abb2813095dd0227ffe9
XAKEPEHOK <icq-475204@ya.ru>
2019-07-31 17:00:48 +0300

    PluginPurpose

---
+++ src/PluginPurpose.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Datetime: 31.07.2019 16:54
+ * @author Timur Kasumov aka XAKEPEHOK
+ */
+
+namespace Leadvertex\Plugin\Components\Purpose;
+
+
+class PluginPurpose
+{
+
+    /**
+     * @var PluginClass
+     */
+    private $class;
+    /**
+     * @var PluginEntity
+     */
+    private $entity;
+
+    public function __construct(PluginClass $class, PluginEntity $entity)
+    {
+        $this->class = $class;
+        $this->entity = $entity;
+    }
+
+    /**
+     * @return PluginClass
+     */
+    public function getClass(): PluginClass
+    {
+        return $this->class;
+    }
+
+    /**
+     * @return PluginEntity
+     */
+    public function getEntity(): PluginEntity
+    {
+        return $this->entity;
+    }
+
+    public function isEquals(?self $purpose): bool
+    {
+        if ($purpose === null) {
+            return false;
+        }
+
+        $isSameClass = $this->class->isEquals($purpose->getClass());
+        $isSameEntity = $this->entity->isEquals($purpose->getEntity());
+        return $isSameClass && $isSameEntity;
+    }
+
+}
